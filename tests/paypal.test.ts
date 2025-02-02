359f0cf27858f9c4d64cce561a76d53d630f39b7
VaheAA <vahe.abov@gmail.com>
2025-02-02 23:23:44 +0400

    add paypal

---
+++ tests/paypal.test.ts
@@ -0,0 +1,11 @@
+import { generateAccessToken } from '../lib/paypal'
+
+test('generates token from paypal', async () => {
+  const tokenResponse = await generateAccessToken()
+
+  console.log({ tokenResponse })
+
+  expect(tokenResponse).toBeDefined()
+  expect(typeof tokenResponse).toBe('string')
+  expect(tokenResponse.length).toBeGreaterThan(0)
+})
